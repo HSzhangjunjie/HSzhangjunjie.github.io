ce88acb967fb48d54f28a68147e956042371a6d6
Jieker <hpzhangjunjiell@163.com>
2020-09-03 09:47:19 +0800

    Site updated: 2020-09-03 09:47:16

---
+++ js/daovoice.js
@@ -0,0 +1,6 @@
+'use strict';
+!function (t, e, o, c, i, d, a) {
+    t.DaoVoiceObject = i, t[i] = t[i] || function () {
+        (t[i].q = t[i].q || []).push(arguments);
+    }, t[i].l = 1 * new Date(), d = e.createElement(o), a = e.getElementsByTagName(o)[0], d.async = 1, d.src = c, d.charset = 'utf-8', a.parentNode.insertBefore(d, a);
+}(window, document, 'script', ('https:' == document.location.protocol ? 'https:' : 'http:') + '//widget.daovoice.io/widget/b9bd0fd6.js', 'daovoice'), daovoice('init', { app_id: 'b9bd0fd6' }), daovoice('update');
